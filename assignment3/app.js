7df6077991880f5ef987fd51b30fd3b594f09499
JakubWienzek <contact@jakubwienzek.eu>
2017-07-21 15:05:07 +0200

    More code. Http working, removing working.

--- assignment3/app.js
+++ assignment3/app.js
@@ -10,41 +10,64 @@
     function NarrowItDownController($scope, MenuSearchService) {
         var ctrl = this;
         var service = MenuSearchService;
+        ctrl.menu = service.getMatchedMenuItems();
 
-        ctrl.foodList = service.getMatchedMenuItems(ctrl.narrowString);  
+        ctrl.removeItem = function(itemIndex) {
+            console.log(itemIndex);
+            ctrl.menu.splice(itemIndex, 1);
+            console.log(ctrl.menu.length);
 
-    };
-
-    MenuSearchService.$inject = ['$http'];
-    function MenuSearchService($http) {
-        var serv = this;
-
-        var found = [];
-        //list excluded
-        //zachowanie asynchronizne $html
-
-        serv.getMatchedMenuItems = function(searchTerm) {
-            console.log("term: " +searchTerm);
-          //That method will be responsible for reaching out to the server (using the $http service)  
-          //https://davids-restaurant.herokuapp.com/menu_items.json
-        };
-
-        serv.removeUnwanted = function(index) {
-            //this will remove unwanted element from list
         };
     };
 
     function FoundItems() {
         var ddo = {
             scope: {
-                //TODO
-                onRemove: "=?",
-                foundItem: "=?",
+                onRemove: "&",
                 found: "<"
             },
-            templateUrl: "components/foundItems.html"
+            templateUrl: "components/foundItems.html",
+            controller: NarrowItDownController,
+            controllerAs: 'narrowCtrl',
+            bindToController: true
         };
 
         return ddo;
+    };
+
+    MenuSearchService.$inject = ['$http'];
+    function MenuSearchService($http) {
+        var serv = this;
+        serv.found = [];
+        serv.menu = [];
+
+        serv.getMatchedMenuItems = function(searchTerm) {
+            var response = $http({
+                method: "GET",
+                url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+            })
+                .then(function (response) {
+                serv.menu = response.data.menu_items;
+                console.log(serv.menu);
+                getFiltederList(searchTerm, serv.menu);
+            })
+                .catch(function (error) {
+                    console.log("Error while getting menu from server");
+                });
+
+            return serv.found;
+        };
+
+        function getFiltederList(searchTerm, menu) {
+            searchTerm = 'p';
+            for(var i=0;i<menu.length;i++) {
+                if(menu[i].name.toLowerCase().indexOf('') !== -1)
+                    serv.found.push(menu[i]);
+            }
+        }
+
+        serv.removeUnwanted = function(index) {
+            //this will remove unwanted element from list
+        };
     };
 })();
