943f101858241316c31709c2edc0ed2feaf3d903
JakubWienzek <contact@jakubwienzek.eu>
2017-07-24 11:29:56 +0200

    Close... 'found' list can not be cleaned and Warning is on screen prior to search

--- assignment3/app.js
+++ assignment3/app.js
@@ -9,74 +9,83 @@
     NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
     function NarrowItDownController($scope, MenuSearchService) {
         var ctrl = this;
-        var service = MenuSearchService;
-        ctrl.menu = function(narrowString) {
-            var promise = service.getMatchedMenuItems();
+        var service = MenuSearchService
+        
+        ctrl.items = service.getFoundItems();
 
-            promise.then(function (response) {
-                console.log(response.data);
-            })
-                .catch(function (error) {
-                    console.log(error);
-                })
+        ctrl.search = function() {
+            service.getMatchedMenuItems(ctrl.narrowString);
         }
 
         ctrl.removeItem = function(itemIndex) {
-            console.log(itemIndex);
-            ctrl.menu.splice(itemIndex, 1);
-            console.log(ctrl.menu.length);
-
+            service.removeItem(itemIndex);
         };
     };
 
     MenuSearchService.$inject = ['$http'];
     function MenuSearchService($http) {
         var serv = this;
-        serv.found = [];
-        serv.menu = [];
+        var found = [];
 
-        serv.getMatchedMenuItems = function() {
+        serv.getFoundItems = function() {
+            return found;
+        }
+
+        serv.removeItem = function(itemIndex) {
+            found.splice(itemIndex, 1);
+        }
+
+        serv.getMatchedMenuItems = function(searchTerm) {
+            
             var response = $http({
                 method: "GET",
                 url: "https://davids-restaurant.herokuapp.com/menu_items.json"
             })
-                .then(function (response) {
-                //serv.menu = response.data.menu_items;
-                console.log("Service: " + serv.menu);
-                //getFiltederList(searchTerm, serv.menu);
+            .then(function (response) {
+                found = [];
+                console.log(found);
+                var menu = response.data.menu_items;
+                setFiltederList(searchTerm, menu);
             })
-                .catch(function (error) {
-                    console.log("Error while getting menu from server");
-                });
-
-            return response;
+            .catch(function (error) {
+                console.log("Error while getting menu from server");
+            });
         };
 
-        function getFiltederList(searchTerm, menu) {
-            searchTerm = 'p';
+        function setFiltederList(searchTerm, menu) {
             for(var i=0;i<menu.length;i++) {
-                if(menu[i].name.toLowerCase().indexOf('') !== -1)
-                    serv.found.push(menu[i]);
+                if(menu[i].name.toLowerCase().indexOf(searchTerm) !== -1)
+                    found.push(menu[i]);
             }
+            console.log("searchTerm: " + searchTerm);
+            console.log(menu);
+            console.log(found);
         }
-
-        serv.removeUnwanted = function(index) {
-            //this will remove unwanted element from list
-        };
     };
 
     function FoundItems() {
         var ddo = {
             scope: {
                 onRemove: "&",
-                found: "<"
+                items: "<"
             },
             templateUrl: "components/foundItems.html",
-            controller: NarrowItDownController,
-            controllerAs: 'narrowCtrl',
+            controller: FoundItemsController,
+            controllerAs: "FIctrl",
             bindToController: true
         };
 
         return ddo;
     };
+
+    function FoundItemsController() {
+        var FIctrl = this;
+
+        FIctrl.emptyList = function() {
+            if(FIctrl.items.length === 0) {
+                return true;
+            }
+            return false;
+        }
+    }
 })();
