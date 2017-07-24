2a689a42bfb3654393fe2369d9b7646a3a62475b
JakubWienzek <contact@jakubwienzek.eu>
2017-07-24 09:05:47 +0200

    More code.

--- assignment3/app.js
+++ assignment3/app.js
@@ -10,13 +10,58 @@
     function NarrowItDownController($scope, MenuSearchService) {
         var ctrl = this;
         var service = MenuSearchService;
-        ctrl.menu = service.getMatchedMenuItems();
+        ctrl.menu = function(narrowString) {
+            var promise = service.getMatchedMenuItems();
+
+            promise.then(function (response) {
+                console.log(response.data);
+            })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
 
         ctrl.removeItem = function(itemIndex) {
             console.log(itemIndex);
             ctrl.menu.splice(itemIndex, 1);
             console.log(ctrl.menu.length);
 
+        };
+    };
+
+    MenuSearchService.$inject = ['$http'];
+    function MenuSearchService($http) {
+        var serv = this;
+        serv.found = [];
+        serv.menu = [];
+
+        serv.getMatchedMenuItems = function() {
+            var response = $http({
+                method: "GET",
+                url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+            })
+                .then(function (response) {
+                //serv.menu = response.data.menu_items;
+                console.log("Service: " + serv.menu);
+                //getFiltederList(searchTerm, serv.menu);
+            })
+                .catch(function (error) {
+                    console.log("Error while getting menu from server");
+                });
+
+            return response;
+        };
+
+        function getFiltederList(searchTerm, menu) {
+            searchTerm = 'p';
+            for(var i=0;i<menu.length;i++) {
+                if(menu[i].name.toLowerCase().indexOf('') !== -1)
+                    serv.found.push(menu[i]);
+            }
+        }
+
+        serv.removeUnwanted = function(index) {
+            //this will remove unwanted element from list
         };
     };
 
@@ -33,41 +78,5 @@
         };
 
         return ddo;
-    };
-
-    MenuSearchService.$inject = ['$http'];
-    function MenuSearchService($http) {
-        var serv = this;
-        serv.found = [];
-        serv.menu = [];
-
-        serv.getMatchedMenuItems = function(searchTerm) {
-            var response = $http({
-                method: "GET",
-                url: "https://davids-restaurant.herokuapp.com/menu_items.json"
-            })
-                .then(function (response) {
-                serv.menu = response.data.menu_items;
-                console.log(serv.menu);
-                getFiltederList(searchTerm, serv.menu);
-            })
-                .catch(function (error) {
-                    console.log("Error while getting menu from server");
-                });
-
-            return serv.found;
-        };
-
-        function getFiltederList(searchTerm, menu) {
-            searchTerm = 'p';
-            for(var i=0;i<menu.length;i++) {
-                if(menu[i].name.toLowerCase().indexOf('') !== -1)
-                    serv.found.push(menu[i]);
-            }
-        }
-
-        serv.removeUnwanted = function(index) {
-            //this will remove unwanted element from list
-        };
     };
 })();
